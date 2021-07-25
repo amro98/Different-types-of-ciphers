0f86a8d66d4813f0a98c1818e827ed2517916253
amro98 <75433739+amro98@users.noreply.github.com>
2021-07-25 15:26:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,497 +1,158 @@
-#include<iostream>
-#include<conio.h>
-#include<vector>
-#include<map>
-#include<string.h>
-#include<stdlib.h>
+#include <iostream>
+
 using namespace std;
-class CaesarCipher
-{
 
-public:
-	string t;
+string message;
+string mappedKey;
 
-	string Encreption(int key, string s)
-	{
-        string t;
-		for (int i = 0; i < s.size(); i++)
-		{
-			t += (s[i] - 96+ key) % 26 + 96;
+void messageAndKey(){
+    string msg;
+    cout << "Enter message: ";
+    getline(cin, msg);
+    cin.ignore();
 
-		}
-
-		return t;
-	}
-
-	string decription(int key, string s)
-	{
-		string t;
-		for (int i = 0; i < s.size(); i++)
-		{
-			t += (s[i] - 96- key + 26) % 26 + 96;
-		}
-		return t;
-	}
-
-
-};
-class Affine_Cipher
-{
-    public:
-    string Affine_Enc(string Msg)
-    {
-        string CTxt = "";
-        int a = 3;
-        int b = 6;
-        for (int i = 0; i < Msg.length(); i++)
-        {
-            CTxt = CTxt + (char) ((((a * Msg[i]) + b) % 26) + 65);
-        }
-        return CTxt;
+    //message to uppercase
+    for(int i = 0; i < msg.length(); i++){
+        msg[i] = toupper(msg[i]);
     }
-    string Affine_Dec(string CTxt )
-    {
 
-
-        string Msg = "";
-        int a = 3;
-        int b = 6;
-        int a_inv = 0;
-        int flag = 0;
-        for (int i = 0; i < 26; i++)
-        {
-            flag = (a * i) % 26;
-            if (flag == 1)
-            {
-                a_inv = i;
-            }
-        }
-        for (int i = 0; i < CTxt.length(); i++)
-        {
-            Msg = Msg + (char) (((a_inv * ((CTxt[i] - b)) % 26)) + 65);
-        }
-        return Msg;
-    }
-};
-class Vigenere
-{
-public:
     string key;
-    Vigenere(string key)
-    {
-        for (int i=0; i<key.size() ; i++)
-        {
-            if(key[i]>='A' && key[i]<='Z')
-                this->key += key[i];
-            else if(key[i]>='a'&&key[i]<='z')
-                this->key +=key[i]+'A'-'a';
+    cout << "Enter key: ";
+    getline(cin, key);
+    cin.ignore();
+
+    //key to uppercase
+    for(int i = 0; i < key.length(); i++){
+        key[i] = toupper(key[i]);
+    }
+
+    //mapping key to message
+    string keyMap = "";
+    for (int i = 0,j = 0; i <msg.length();i++){
+        if(msg[i] ==32){
+            keyMap += 32;
+        } else {
+            if(j<key.length()){
+                keyMap += key[j];
+                j++;
+            } else {
+                j = 0;
+                keyMap += key[j];
+                j++;
+            }
+        } //if-else
+    } //for
+
+//    cout << msg << "\n" << keyMap;
+    message = msg;
+    mappedKey = keyMap;
+}
+
+int tableArr[26][26];
+void createVigenereTable(){
+    for (int i = 0; i < 26; i++){
+        for (int j = 0; j < 26; j++){
+            int temp;
+            if((i+65)+j > 90){
+                temp = ((i+65)+j) - 26;
+
+                //adding ASCII of alphabet letter in table index position
+                tableArr[i][j] = temp;
+            } else {
+                temp = (i+65)+j;
+
+                //adding ASCII of alphabet letter in table index position
+                tableArr[i][j] = temp;
+            }
+        } // for j loop
+    } // for i loop
+
+    // testing the table
+    for(int i = 0; i <26; i++){
+        for(int j = 0; j < 26; j++){
+            cout << (char)tableArr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void cipherEncryption(string message, string mappedKey){
+    createVigenereTable();
+    string encryptedText = "";
+    for(int i = 0; i < message.length(); i++){
+        if(message[i] == 32 && mappedKey[i] == 32){
+            encryptedText += " ";
+        } else {
+            int x = (int)message[i]-65;
+            int y = (int)mappedKey[i]-65;
+            encryptedText += (char)tableArr[x][y];
         }
     }
-    string Encrypt(string text)
-    {
-        string out;
-        for(int i=0 ,j=0; i<text.length();i++)
-            {
-                    char c = text[i];
-                    if (c >= 'a' && c <= 'z')
-                        c += 'A' - 'a';
-                    else if (c < 'A' || c > 'Z')
-                        continue;
-                    out += (c + key[j] - 2 * 'A') % 26 + 'A';
-                    j = (j + 1) % key.length();
-            }
-        return out;
-    }
-        string decryption(string t)
-        {
-		string output;
-		for (int i = 0, j = 0; i < t.length(); ++i)
-        {
-			char c = t[i];
-			if (c >= 'a' && c <= 'z')
-				c += 'A' - 'a';
-			else if (c < 'A' || c > 'Z')
-				continue;
-			output += (c - key[j] + 26) % 26 + 'A';
-			j = (j + 1) % key.length();
-		}
-		return output;
+
+    cout << "Encrypted Text: " << encryptedText;
+}
+
+int itrCount(int key, int msg){
+    int counter = 0;
+    string result = "";
+
+    //starting from ASCII of letter of Key and ending at letter of message
+    // to get full 26 letters of alphabet
+    for(int i = 0; i < 26; i++){
+        if(key+i > 90){
+            result += (char)(key+(i-26));
+        } else {
+            result += (char)(key+i);
         }
+    } //for
 
-
-};
-class Vernam_Cipher
-{
-
-public:
-
-	string encreption(string key, string m)
-	{
-		int i, j;
-		int mod = key.size();
-		j = 0;
-		for (i = key.size(); i<m.size(); i++) {
-			key += key[j%mod];
-			j++;
-		}
-		string ans = "";
-		for (i = 0; i<m.size(); i++) {
-			ans += (key[i] - 'A' + m[i] - 'A') % 26 + 'A';
-		}
-
-		return ans;
-	}
-	string decreption(string key, string m)
-	{
-		int i, j;
-		int mod = key.size();
-		j = 0;
-		for (i = key.size(); i<m.size(); i++) {
-			key += key[j%mod];
-			j++;
-		}
-		string ans = "";
-		for (i = 0; i<m.size(); i++) {
-			ans += (m[i] - key[i] + 26) % 26 + 'A';
-		}
-
-
-		return ans;
-	}};
-
-class Playfair_Cipher
-{
-public:
-
-void SelectPlayfair_CipherTyepDecryption() {
-
-    int i, j, k, n;
-    cout << "Enter the encrypted message\n" << endl;
-    string s;
-    cin >> s;
-    cout << "Enter the key\n" << endl;
-    string key;
-    cin >> key;
-    vector<vector<char> > a(5, vector<char>(5, ' '));
-    n = 5;
-    map<char, int> mp;
-    k = 0;
-    int pi, pj;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            while (mp[key[k]] > 0 && k < key.size()) {
-                k++;
-            }
-            if (k < key.size()) {
-                a[i][j] = key[k];
-                mp[key[k]]++;
-                pi = i;
-                pj = j;
-            }
-            if (k == key.size())
-                break;
-        }
-        if (k == key.size())
+    for(int i = 0; i < result.length(); i++){
+        if(result[i] == msg){
             break;
-    }
-    k = 0;
-    for (; i < n; i++) {
-        for (; j < n; j++) {
-            while (mp[char(k + 'a')] > 0 && k < 26) {
-                k++;
-            }
-            if (char(k + 'a') == 'j') {
-                j--;
-                k++;
-                continue;
-            }
-            if (k < 26) {
-                a[i][j] = char(k + 'a');
-                mp[char(k + 'a')]++;
-            }
-        }
-        j = 0;
-    }
-    string ans;
-    map<char, pair<int, int> > mp2;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            mp2[a[i][j]] = make_pair(i, j);
+        } else {
+            counter++;
         }
     }
-    for (i = 0; i < s.size() - 1; i += 2) {
-        int y1 = mp2[s[i]].first;
-        int x1 = mp2[s[i]].second;
-        int y2 = mp2[s[i + 1]].first;
-        int x2 = mp2[s[i + 1]].second;
-        if (y1 == y2) {
-            ans += a[y1][(x1 - 1) % 5];
-            ans += a[y1][(x2 - 1) % 5];
-        }
-        else if (x1 == x2) {
-            ans += a[(y1 - 1) % 5][x1];
-            ans += a[(y2 - 1) % 5][x2];
-        }
-        else {
-            ans += a[y1][x2];
-            ans += a[y2][x1];
-        }
-    }
-    if (ans[ans.size() - 1] == 'x')
-        ans[ans.size() - 1] = '\0';
-    for (i = 1; i < ans.size(); i++) {
-        if (ans[i] == 'x')
-            ans[i] = ans[i - 1];
-    }
-
-    cout << ans << '\n' << endl;
-
-}
-void SelectPlayfair_CipherTyepEncryption() {
-
-    int i, j, k, n;
-    cout << "Enter the message" << endl;
-    string s, origin;
-   cin>> origin;
-    cout << "Enter the key" << endl;
-    string key;
-    cin >> key;
-    for (i = 0; i < origin.size(); i++) {
-        if (origin[i] != ' ')
-            s += origin[i];
-    }
-    vector<vector<char> > a(5, vector<char>(5, ' '));
-    n = 5;
-    map<char, int> mp;
-    k = 0;
-    int pi, pj;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            while (mp[key[k]] > 0 && k < key.size()) {
-                k++;
-            }
-            if (k < key.size()) {
-                a[i][j] = key[k];
-                mp[key[k]]++;
-                pi = i;
-                pj = j;
-            }
-            if (k == key.size())
-                break;
-        }
-        if (k == key.size())
-            break;
-    }
-    k = 0;
-    for (; i < n; i++) {
-        for (; j < n; j++) {
-            while (mp[char(k + 'a')] > 0 && k < 26) {
-                k++;
-            }
-            if (char(k + 'a') == 'j') {
-                j--;
-                k++;
-                continue;
-            }
-            if (k < 26) {
-                a[i][j] = char(k + 'a');
-                mp[char(k + 'a')]++;
-            }
-        }
-        j = 0;
-    }
-    string ans;
-    if (s.size() % 2 == 1)
-        s += "x";
-    for (i = 0; i < s.size() - 1; i++) {
-        if (s[i] == s[i + 1])
-            s[i + 1] = 'x';
-    }
-    map<char, pair<int, int> > mp2;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            mp2[a[i][j]] = make_pair(i, j);
-        }
-    }
-
-    for (i = 0; i < s.size() - 1; i += 2) {
-        int y1 = mp2[s[i]].first;
-        int x1 = mp2[s[i]].second;
-        int y2 = mp2[s[i + 1]].first;
-        int x2 = mp2[s[i + 1]].second;
-        if (y1 == y2) {
-            ans += a[y1][(x1 + 1) % 5];
-            ans += a[y1][(x2 + 1) % 5];
-        }
-        else if (x1 == x2) {
-            ans += a[(y1 + 1) % 5][x1];
-            ans += a[(y2 + 1) % 5][x2];
-        }
-        else {
-            ans += a[y1][x2];
-            ans += a[y2][x1];
-        }
-    }
-    cout << ans << '\n' << endl;
-
+    return counter;
 }
 
-};
 
-void Display_All_Encryption()
-{
-    int Key,Choice,x;
-    string M,Msg,St_key,k;
-    CaesarCipher obj1;
-    Affine_Cipher obj2;
-    Vernam_Cipher obj4;
-    Playfair_Cipher obj5;
+void cipherDecryption(string message, string mappedKey){
+    string decryptedText = "";
+    for(int i = 0; i < message.length(); i++){
+        if(message[i] == 32 && mappedKey[i] == 32){
+            decryptedText += " ";
+        } else {
+            int temp = itrCount((int)mappedKey[i], (int)message[i]);
+            decryptedText += (char)(65+temp);
+        }
+    }
 
-
-                cout<<"Press 1 For Ceaser Cipher Encryption\n";
-                cout<<"Press 2 For Affine Cipher Encryption\n";
-                cout<<"Press 3 For Vigenere Cipher Encryption\n";
-                cout<<"Press 4 For Vernam Cipher Encryption\n";
-                cout<<"Press 5 For Playfair Cipher Encryption\n";
-                cout<<"------Press 0 to Exit------\n";
-                cin>>Choice;
-                if(Choice==1)
-                {
-                    cout << "Enter The Key" << endl;
-                    cin >> Key;
-                    cout << "Enter The Msg" << endl;
-                    cin >> Msg;
-                    cout << "the Encreption is: " << obj1.Encreption(Key,Msg) << endl;
-                }
-                else
-                    if(Choice==2)
-                {
-
-                    cout << "Enter The Msg" << endl;
-                    cin >> Msg;
-                    cout << "the Encreption is " << obj2.Affine_Enc(Msg) << endl;
-
-                }
-                else
-                    if(Choice==3)
-                {cout << "enter the msg" << endl;
-                    string ori;
-                    cin >> ori;
-                    cout<<"Enter The Key"<<endl;
-                    cin>>k;
-                    Vigenere obj3(k);
-                    cout << "the incription is " << obj3.Encrypt(ori)<<endl;
-                }
-                else
-                if(Choice==4)
-                {
-                    cout << "Enter The String Key" << endl;
-                    cin >> St_key;
-                    cout << "Enter The Msg" << endl;
-                    cin>>Msg;
-                    cout << "the Encreption is \t" << obj4.encreption(St_key,Msg) << endl;
-                }
-                else
-                    if(Choice==5)
-                {
-                    obj5.SelectPlayfair_CipherTyepEncryption();
-                }
-                else
-                {
-                    exit(0);
-                }
-
-
-}
-void Display_All_Decryption()
-{
-    CaesarCipher obj1;
-    Affine_Cipher obj2;
-    Vernam_Cipher obj4;
-    Playfair_Cipher obj5;
-    int Key,Choice,x;
-    string M,Msg,St_key;
-
-                    cout<<"Press 1 For Ceaser Cipher Decryption\n";
-                    cout<<"Press 2 For Affine Cipher Decryption\n";
-                    cout<<"Press 3 For Vernam Cipher Decryption\n";
-                    cout<<"Press 4 For Playfair Cipher Decryption\n";
-                    cout<<"Press 5 For Viggner Cipher Decryption\n";
-                    cout<<"------Press 0 to Exit------\n";
-                    cin>>Choice;
-                if(Choice==1)
-                {
-                    cout << "Enter The Key" << endl;
-                    cin >> Key;
-                    cout << "Enter The Msg" << endl;
-                    cin >> Msg;
-                    cout << "the Decrption is \t" << obj1.decription(Key,Msg) << endl;
-
-                }
-                else
-                    if(Choice==2)
-                {
-
-                    cout << "Enter The Msg" << endl;
-                    cin >> Msg;
-                    cout << "the Decrption is " << obj2.Affine_Dec(Msg) << endl;
-
-                }
-                else
-                    if(Choice==3)
-                    {
-                        cout << "Enter The String Key" << endl;
-                        cin >> St_key;
-                        cout << "Enter The Msg" << endl;
-                        cin>>Msg;
-                        cout << "the Decrption is \t" << obj4.decreption(St_key,Msg) << endl;
-                    }
-                else
-                    if(Choice==4)
-                {
-                    obj5.SelectPlayfair_CipherTyepEncryption();
-                }
-                else if(Choice==5)
-                {
-                    cout << "enter the msg" << endl;
-                    string ori,k;
-                    cin >> ori;
-                    cout<<"Enter The Key"<<endl;
-                    cin>>k;
-                    Vigenere obj3(k);
-                    cout << "the Decrption is " << obj3.decryption(ori)<<endl;
-                }
-                else
-                {
-                    exit(0);
-                }
-
-
+    cout << "Decrypted Text: " << decryptedText;
 }
 
 
 int main()
 {
-    int x;
-    for(int j = 0; j<100 ; j++)
-    {
+    cout << "Message and key can only be alphabetic" << endl;
+    int choice;
+    cout << "1. Encryption\n2. Decryption\nChoose(1,2): ";
+    cin >> choice;
+    cin.ignore();
 
-        cout<<"---(1)Encrypt Or (2)Decrypt---\n"<<endl;
-        cin>>x;
-        if(x==1)
-        {
-            Display_All_Encryption();
-        }
-        else if(x==2)
-        {
-            Display_All_Decryption();
-        }
+    if (choice == 1){
+        cout << "Encryption" << endl;
+        messageAndKey();
+        cipherEncryption(message, mappedKey);
 
-        else
-            {return 0;}
+    } else if (choice == 2){
+        cout << "Decryption" << endl;
+        messageAndKey();
+        cipherDecryption(message, mappedKey);
+
+    } else {
+        cout << "Wrong Choice" << endl;
     }
 
-
     return 0;
-
 }
